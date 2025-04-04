856bc7f682b14f643897900f837829d287b42bad
kat9-rud <kat9.rud@gmail.com>
2025-04-04 11:16:36 +0200

    added allure steps for InternationalRouteTests

--- transit-time-calc-tests/src/test/java/com/gmail/ek/rudenko/InternationalRouteTests.java
+++ transit-time-calc-tests/src/test/java/com/gmail/ek/rudenko/InternationalRouteTests.java
@@ -2,13 +2,11 @@ package com.gmail.ek.rudenko;
 
 import com.gmail.ek.rudenko.model.TestCaseData;
 import com.gmail.ek.rudenko.util.PostcodeResolver;
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Severity;
+import io.qameta.allure.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import static io.qameta.allure.Allure.*;
 import static io.qameta.allure.SeverityLevel.CRITICAL;
 import static io.qameta.allure.SeverityLevel.NORMAL;
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,18 +19,29 @@ public class InternationalRouteTests extends BaseTest {
     @ParameterizedTest(name = "{0}")
     @MethodSource("com.gmail.ek.rudenko.util.TestCaseDataLoader#successfulCasesInternational")
     public void testSuccessfulCalculation(TestCaseData tc) {
-        calculatorPage.open()
-                .selectOriginCountry(tc.getOriginCountry())
-                .enterOriginPostcode(PostcodeResolver.resolve(tc.getOriginPostcode(), tc.getOriginCountry()))
-                .selectDestinationCountry(tc.getDestinationCountry())
-                .enterDestinationPostcode(PostcodeResolver.resolve(tc.getDestinationPostcode(), tc.getDestinationCountry()))
-                .clickCalculateButton();
-        assertTrue(calculatorPage.isTransitTimeResultSectionVisible(), "Transit Time Result should be shown.");
-        assertEquals(2, calculatorPage.getProductOptionCount(), "Expected 2 product options for international route");
-        assertFalse(calculatorPage.isDeliveryDatePickerVisible(), "Delivery date picker should not be visible for international route.");
-        assertTrue(calculatorPage.isPickupDatePickerVisible(), "Expected pickup date picker for international route.");
-        calculatorPage.clickEditPreviousStepButton();
-        assertFalse(calculatorPage.isTransitTimeResultSectionVisible(), "Transit Time Result section should NOT be shown.");
+        step("Fill out and submit Transit Time Calculator form", () -> {
+            calculatorPage.open()
+                    .selectOriginCountry(tc.getOriginCountry())
+                    .enterOriginPostcode(PostcodeResolver.resolve(tc.getOriginPostcode(), tc.getOriginCountry()))
+                    .selectDestinationCountry(tc.getDestinationCountry())
+                    .enterDestinationPostcode(PostcodeResolver.resolve(tc.getDestinationPostcode(), tc.getDestinationCountry()))
+                    .clickCalculateButton();
+        });
+
+        step("Verify Transit Time Result section and product options", () -> {
+            assertTrue(calculatorPage.isTransitTimeResultSectionVisible(), "Transit Time Result should be shown.");
+            assertEquals(2, calculatorPage.getProductOptionCount(), "Expected 2 product options for international route");
+        });
+
+        step("Check visibility of date pickers", () -> {
+            assertFalse(calculatorPage.isDeliveryDatePickerVisible(), "Delivery date picker should not be visible for international route.");
+            assertTrue(calculatorPage.isPickupDatePickerVisible(), "Expected pickup date picker for international route.");
+        });
+
+        step("Go back and check that Transit Time Result section disappears", () -> {
+            calculatorPage.clickEditPreviousStepButton();
+            assertFalse(calculatorPage.isTransitTimeResultSectionVisible(), "Transit Time Result section should NOT be shown.");
+        });
     }
 
     @Severity(NORMAL)
